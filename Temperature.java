427fe783fa5206e4ef8e19559d76c80ffea36e46
iyashreyar <yashreyar5143@gmail.com>
2026-03-06 13:54:10 +0530

    New file added

---
+++ Temperature.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter temprature in C: ");
+        float tempC = sc.nextFloat();
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println(tempC + "'C in fahrenheit is " + tempF + "'F");
+    }
+}
