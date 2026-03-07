69205b75e0537c377bb773bb913e52f7ab7937b7
iyashreyar <yashreyar5143@gmail.com>
2026-03-07 15:00:04 +0530

    Cound digit occurences in number question solved

---
+++ CountNums.java
@@ -0,0 +1,15 @@
+public class CountNums {
+    public static void main(String[] args) {
+        int count = 0;
+        int num = 1237325;
+        int i = 8;
+        while(num > 0){
+            int rem = num%10;
+            if(rem == i){
+                count++;
+            }
+            num = num/10;
+        }
+        System.out.println(count);
+    }
+}
