dee7896984e31eed500e0f00080826d4d7d772c7
iyashreyar <yashreyar5143@gmail.com>
2026-03-11 11:24:17 +0530

    Multi arraylist basics done

---
+++ MultiAL.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiAL {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        // Initialisation
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // Add values/elements
+        System.out.print("Input elements: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(sc.nextInt());
+            }
+        }
+
+        System.out.println(list);
+    }
+}
