0856b17ee944c743a5e01cb8274ef9d805c679e5
iyashreyar <yashreyar5143@gmail.com>
2026-03-12 11:22:46 +0530

    Permanently remove compiled .class files and ignore them

---
+++ SmallestLetter.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+public class SmallestLetter {
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'c';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int s = 0;
+        int e = letters.length-1;
+
+        while(s <= e){
+            int m = s + (e - s) / 2;
+            if (target >= letters[m]){
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        return letters[s % letters.length];
+    }
+}
