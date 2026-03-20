4a1b7b8f4d21af4e191e3e79fc21b400e813f2e4
iyashreyar <yashreyar5143@gmail.com>
2026-03-20 12:55:55 +0530

    Selection sort code done

---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,3,7,6,1,4,2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            // Find the maximum item in the remaining array and swap it with the correct index
+            int last = arr.length-i-1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+    static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if(arr[i] > arr[max]){
+                max = i;
+            }
+        }
+        return max;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
