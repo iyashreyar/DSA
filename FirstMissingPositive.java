f4819a4bfa3fc84b8ce1397915bb697ce11b1fd8
iyashreyar <yashreyar5143@gmail.com>
2026-03-24 14:33:27 +0530

    First missing positive number ques solved

---
+++ FirstMissingPositive.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/first-missing-positive/description/
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] arr= {3,4,-1,1};
+        System.out.println(firstMissingPositive(arr));
+    }
+    static int firstMissingPositive(int[] arr) {
+        int i = 0;
+        while(i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if(arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        // Provide the first missing positive value 
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index+1){
+                return index+1;
+            }
+        }
+        return arr.length+1;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
